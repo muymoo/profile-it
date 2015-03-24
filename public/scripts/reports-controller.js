a56d61365c86a2ab1948415bb53435099c8afeee
lrbridge <lrbridge@users.noreply.github.com>
2015-03-24 00:43:20 -0700

    sip to find only actions from last day
    … filtering by last day isn’t quite working yet though

--- public/scripts/reports-controller.js
+++ public/scripts/reports-controller.js
@@ -1,25 +1,39 @@
 profilerApp.controller('ReportsController', ['$scope', '$http', function($scope, $http){
 	
-	$scope.myData = [];
-
+	$scope.last25Data = [];
+	$scope.last25 = { data: 'last25Data' };
 	$http.get('/all/last25').success(function(last25) {
-
-		console.log(last25);
 		for(var idx in last25) {
 			var one = last25[idx];
-			$scope.myData.push(
+			$scope.last25Data.push(
 				{
 					op: one.op,
 					query: one.query,
 					command: one.command,
 					nscannedObjects: one.nscannedObjects,
-					responseLength: one.responseLength
+					responseLength: one.responseLength,
+					timestamp: one.ts
 				}
 			);
 		}
-
 	});
 
-	$scope.myOptions = { data: 'myData' };
-	
+	$scope.lastDayData = [];
+	$scope.lastDay = { data: 'lastDayData' };
+	$http.get('/all/lastDay').success(function(lastDay) {
+		for(var idx in lastDay) {
+			var one = lastDay[idx];
+			$scope.lastDayData.push(
+				{
+					op: one.op,
+					query: one.query,
+					command: one.command,
+					nscannedObjects: one.nscannedObjects,
+					responseLength: one.responseLength,
+					timestamp: one.ts
+				}
+			);
+		}
+	});
+
 }]);
--- server/controllers/alloperations.js
+++ server/controllers/alloperations.js
@@ -4,22 +4,55 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var connection = mongoose.connection;
 
-	//db.system.profile.find().limit(10).sort( { ts : -1 } ).pretty()
+var SystemProfile = mongoose.mquery(connection.collection('system.profile')).toConstructor();
 
 // Get one run
 router.get('/last25', function(req, res, next) {
+  //db.system.profile.find().limit(10).sort( { ts : -1 } ).pretty()
 
-	var Last25 = mongoose.mquery(connection.collection('system.profile')).toConstructor();
-  	Last25()
-  		.find({}, function(err) {
-  			console.log(err);
-  		})
-  		.limit(25)
-  		.sort( { ts : -1 } )
-  		.exec(function(err, result) {
-  			console.log("RESULT", result);
-  			res.send(result);
-  		});
+  SystemProfile()
+  	.find({}, function(err) {
+  		console.log(err);
+  	})
+  	.limit(25)
+  	.sort( { ts : -1 } )
+  	.exec(function(err, result) {
+  		res.send(result);
+  	});
+});
+
+router.get('/lastDay', function(req, res, next) {
+// db.system.profile.find(
+//                        {
+//                         ts : {
+//                               $gt : new ISODate("2012-12-09T03:00:00Z") ,
+//                               $lt : new ISODate("2012-12-09T03:40:00Z")
+//                              }
+//                        }
+//                       ).sort( { millis : -1 } )
+console.log("IN");
+SystemProfile()
+    .find()
+      // ts: {
+      //   //'$gt' : new Date('2012-03-20T16:00:00.000Z')//,//1234, //new ISODate("2012-12-09T03:00:00Z")
+      //   '$lt' : new Date('2015-03-25T16:00:00.000Z')//5678//new ISODate("2012-12-09T03:40:00Z")
+      // }
+      // op:{
+      //   '$ne' : 'command'
+      // }
+    // }, function(err) {
+    //   console.log("ERRRRR",err);
+    // })
+    .where('ts')
+    .gt(new Date('2015-03-24T16:07:00.000Z')) // TODO this isn't working :( :(
+    .lt(new Date('2015-03-24T16:07:10.000Z'))
+    // .where('op')
+    // .ne('command')
+    .sort( { ts : -1 } )
+    .exec(function(err, result) {
+      console.log("RESULT:", result);
+      res.send(result);
+    });
 });
 
 module.exports = router;
