a3abe6b58bc782cb6a5270c88c0d89524e1d449e
lrbridge <lrbridge@users.noreply.github.com>
2015-03-25 21:13:18 -0700

    getting aggregate data from system.profile

--- public/scripts/reports-controller.js
+++ public/scripts/reports-controller.js
@@ -1,41 +1,18 @@
 profilerApp.controller('ReportsController', ['$scope', '$http', function($scope, $http){
 	
-
-	
-	// $scope.last25Data = [];
-	// $scope.last25 = { data: 'last25Data' };
-	// $http.get('/all/last25').success(function(last25) {
-	// 	for(var idx in last25) {
-	// 		var one = last25[idx];
-	// 		$scope.last25Data.push(
-	// 			{
-	// 				op: one.op,
-	// 				query: one.query,
-	// 				command: one.command,
-	// 				lockStats: one.lockStats,
-	// 				nscannedObjects: one.nscannedObjects,
-	// 				responseLength: one.responseLength,
-	// 				timestamp: one.ts,
-	// 				ns: one.ns,
-	// 				millis: one.millis
-	// 			}
-	// 		);
-	// 	}
-	// });
-
-	$scope.lastDayData = [];
-	$scope.lastDay = { data: 'lastDayData' };
-	$http.get('/all/lastDay').success(function(lastDay) {
+	$scope.aggData = [];
+	$scope.agg = { data: 'aggData' };
+	$http.get('/all/aggLastDay').success(function(lastDay) {
+		console.log(lastDay);
 		for(var idx in lastDay) {
 			var one = lastDay[idx];
-			$scope.lastDayData.push(
+			$scope.aggData.push(
 				{
 					op: one.op,
 					query: one.query,
-					command: one.command,
-					// lockStats: one.lockStats,
-					// nscannedObjects: one.nscannedObjects,
-					// responseLength: one.responseLength,
+					lockStats: one.lockStats,
+					nscannedObjects: one.nscannedObjects,
+					responseLength: one.responseLength,
 					timestamp: one.ts,
 					ns: one.ns,
 					millis: one.millis
@@ -44,4 +21,42 @@ profilerApp.controller('ReportsController', ['$scope', '$http', function($scope,
 		}
 	});
 
+	// $scope.lastDayData = [];
+	// $scope.lastDay = { data: 'lastDayData' };
+	// $http.get('/all/lastDay').success(function(lastDay) {
+	// 	for(var idx in lastDay) {
+	// 		var one = lastDay[idx];
+	// 		$scope.lastDayData.push(
+	// 			{
+	// 				op: one.op,
+	// 				query: one.query,
+	// 				command: one.command,
+	// 				// lockStats: one.lockStats,
+	// 				// nscannedObjects: one.nscannedObjects,
+	// 				// responseLength: one.responseLength,
+	// 				timestamp: one.ts,
+	// 				ns: one.ns,
+	// 				millis: one.millis
+	// 			}
+	// 		);
+	// 		console.log(one);
+	// 	}
+	// });
+
 }]);
+
+/*
+all available things in system.profile 
+http://docs.mongodb.org/manual/reference/database-profiler/#output-reference
+
+potentially interesting:
+op, ns, query (.$query, orderby, etc. for query ops), ntoreturn, nscanned, scanAndOrder, moved, nmoved, nupdated, 
+keyUpdates, lockStats, nreturned, responseLength, millis
+
+maybe?
+command, updateobj, ntoskip
+
+probably not:
+ts, cursorid, client, user
+
+*/
--- server/controllers/alloperations.js
+++ server/controllers/alloperations.js
@@ -6,7 +6,6 @@ var mongoose = require('mongoose');
 var systemProfile = mongoose.model('SystemProfile');
 
 router.get('/lastDay', function(req, res, next) {
-
   var today = new Date();
   var yesterday = new Date();
   yesterday.setDate(today.getDate() - 1);
@@ -16,8 +15,8 @@ router.get('/lastDay', function(req, res, next) {
   systemProfile
       .find({
         'ts': {
-          $gt: new Date('2015-03-24T07:06:00.000Z'),
-          $lt: new Date('2015-03-24T07:08:00.000Z')
+          $gt: yesterday,
+          $lt: today
         }
       })
       // .where('ts') // time range
@@ -25,14 +24,76 @@ router.get('/lastDay', function(req, res, next) {
       // .lt(today)
       // .gt(new Date('2015-03-24T07:06:00.000Z'))
       // .lt(new Date('2015-03-24T07:08:00.000Z'))
-      .where('op') // can ignore certain types of operations 
-      .equals('query')
-      .where('ns')
-      .equals('my_database.system.profile') // limit to only certain collections (dbname.collection) - exclude my_database.system.profile
+      // .where('op') // can ignore certain types of operations 
+      // .equals('query')
+      // .where('ns')
+      // .equals('my_database.system.profile') // limit to only certain collections (dbname.collection) - exclude my_database.system.profile
       .sort( { ts : -1 } ) // descending time
       .exec(function(err, result) {
         res.send(result);
       });
+});
+
+router.get('/aggLastDay', function(req, res, next) {
+// http://docs.mongodb.org/manual/reference/operator/aggregation/group/
+// db.sales.aggregate(
+//    [
+//       {
+//         $group : {
+//            _id : { month: { $month: "$date" }, day: { $dayOfMonth: "$date" }, year: { $year: "$date" } },
+//            totalPrice: { $sum: { $multiply: [ "$price", "$quantity" ] } },
+//            averageQuantity: { $avg: "$quantity" },
+//            count: { $sum: 1 }
+//         }
+//       }
+//    ]
+// )
+// Model
+// .aggregate({ $match: { age: { $gte: 21 }}})
+// .unwind('tags')
+// .exec(callback)
+
+  var today = new Date();
+  var yesterday = new Date();
+  yesterday.setDate(today.getDate() - 1);
+  console.log(today);
+  console.log(yesterday);
+
+  systemProfile
+  // .find()
+      .aggregate()
+      .group({
+        _id: {'ns':'$ns', 'op':'$op'},//'$ns',//null, // ns & op
+        maxMillis: { $max: '$millis' },
+        avgMillis: { $avg: "$millis" },
+        minMillis: { $min: "$millis" },
+        count: { $sum: 1 },
+      })
+      // .select('id maxMillis')
+      // .find({
+      //   'ts': {
+      //     $gt: yesterday,
+      //     $lt: today
+      //   }
+      // })
+      // .where('ts') // time range
+      // .gt(yesterday)
+      // .lt(today)
+      // .gt(new Date('2015-03-24T07:06:00.000Z'))
+      // .lt(new Date('2015-03-24T07:08:00.000Z'))
+      // .where('op') // can ignore certain types of operations 
+      // .equals('query')
+      // .where('ns')
+      // .equals('my_database.system.profile') // limit to only certain collections (dbname.collection) - exclude my_database.system.profile
+      // .sort( { ts : -1 } ) // descending time
+      .exec(function(err, result) {
+        console.log('IN');
+        if (err) {
+          console.log('ERROR',err);
+        }
+        res.send(result);
+      });
+
 });
 
 module.exports = router;
