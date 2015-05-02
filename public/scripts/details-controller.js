9f659c8637f2cc4d14bcbce8937d83713b9de79f
lrbridge <lrbridge@users.noreply.github.com>
2015-05-01 22:59:10 -0700

    added scanned, millis, and response length details graphs

--- public/scripts/details-controller.js
+++ public/scripts/details-controller.js
@@ -1,16 +1,62 @@
 profilerApp.controller('DetailsController', ['$scope', '$http', '$stateParams', '$state', function($scope, $http, $stateParams, $state) {
-	
-	var collection = $stateParams.collection;
-	var operation = $stateParams.operation;
-	var obj = $stateParams.obj;
-	console.log(collection, operation, obj);
+
+	$scope.collection = $stateParams.collection;
+	$scope.operation = $stateParams.operation;
+	$scope.obj = $stateParams.obj;
+	console.log($scope.collection, $scope.operation, $scope.obj);
 
 	$scope.detailsOperationInstanceData = [];
 	$scope.detailsOperationInstance = { data: 'detailsOperationInstanceData' };
-	$http.post('/stats/collection/' + collection + '/operation', {operation: operation, obj: obj}).success(function(result) {
+	$http.post('/stats/collection/' + $scope.collection + '/operation', {operation: $scope.operation, obj: $scope.obj}).success(function(result) {
 		console.log(result);
+
+		var categories = [];
+		var nScannedSeries = [{
+			name: 'Number documents scanned',
+			data: []
+		},{
+			name: 'Number documents scanned in index',
+			data: []
+		},{
+			name: 'Number documents returned',
+			data: []
+		}];
+
+		var responseLengthSeries = [{
+			name: 'Response Length',
+			data: []
+		}];
+
+		var queryTimeSeries = [{
+			name: 'Time to complete query',
+			data: []
+		}];
+
 		for(var index in result) {
 			var item = result[index];
+
+			var categoryString = item.ts;
+						
+			categories.push(categoryString);
+			nScannedSeries[0].data.push(item.nscannedObjects);
+			nScannedSeries[1].data.push(item.nscanned);
+			nScannedSeries[2].data.push(item.nreturned); // could also do ntoskip (also expensive?)  // ntoreturn --> shows if have limit (if 0 no limit)
+			responseLengthSeries[0].data.push(item.responseLength);
+			queryTimeSeries[0].data.push(item.millis);
+
+			// TODO scanAndOrder if can't use index to return order.. must resort
+
+			// (ninserted, ndeleted, nMatched, nModified)
+
+			// keyUpdates
+
+			// writeConflicts
+
+			// numYield
+
+			// locks (acquireCount, acquireWaitCount, timeAcquiringMicros, deadlockCount)
+
+
 			$scope.detailsOperationInstanceData.push(
 				{
 					'Operation': item.op,
@@ -18,10 +64,23 @@ profilerApp.controller('DetailsController', ['$scope', '$http', '$stateParams', 
 					'Update': item.updateobj,
 					'Millis': item.millis,
 					'Lock Stats': item.lockStats,
-					'Number Scanned': item.nscannedObjects,
+					'Number Scanned': item.nscanned,
 					'Response Length': item.responseLength,
 				}
 			);
+		}
+
+		$scope.nscanned = {
+			series: nScannedSeries,
+			categories: categories
+		};
+		$scope.responseLength = {
+			series: responseLengthSeries,
+			categories: categories
+		}
+		$scope.queryTime = {
+			series: queryTimeSeries,
+			categories: categories
 		}
 	});
 
